3b10c8b0c62ddb746624be212d6ddac7c1f980f7
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-17 11:26:50 -0300

    prisma integration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
+import { PrismaService } from './database/prisma.service';
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  public constructor(private readonly prismaService: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/hello')
+  async getHello() {
+    const member = await this.prismaService.teamMember.create({
+      data: {
+        id: 'captain',
+        name: 'Gabriel Santos Cardoso',
+        function: 'Navigate between the tears',
+      },
+    });
+
+    return {
+      member,
+    };
   }
 }
